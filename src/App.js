6488055cd43c5ed03e20104a807ad54678a8a8a3
rasel <russelroy15@gmail.com>
2021-10-03 01:43:36 +0600

    WIP on GrandMa context project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import "./App.css";
 import Grandfather from "./components/Grandfather/Grandfather";
 
 function App() {
+  const [house, setHouse] = useState(1);
   return (
     <div className="App">
-      <Grandfather house="5" />
+      <button onClick={() => setHouse(house + 1)}>Buy a new houses</button>
+      <Grandfather house={house} />
     </div>
   );
 }
