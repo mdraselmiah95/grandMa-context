ff4474c3eb2f4551e607643ed6dbe72ca64e2f5e
rasel <russelroy15@gmail.com>
2021-10-03 01:38:41 +0600

    WIP on GrandMa context project

--- src/components/Aunty/Aunty.js
+++ src/components/Aunty/Aunty.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-const Aunty = () => {
+const Aunty = (props) => {
+  const { house } = props;
   return (
     <div>
       <h2>THis is Aunty.</h2>
+      <h5>House: {house}</h5>
     </div>
   );
 };
--- src/components/Uncle/Uncle.js
+++ src/components/Uncle/Uncle.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-const Uncle = () => {
+const Uncle = (props) => {
+  const { house } = props;
   return (
     <div>
       <h2>This is Uncle.</h2>
+      <h5>House: {house}</h5>
     </div>
   );
 };
