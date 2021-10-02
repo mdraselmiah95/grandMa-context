f072e34b86f25974f5b4efc4ef0e8bd88a3fe157
rasel <russelroy15@gmail.com>
2021-10-03 03:00:44 +0600

    WIP on GrandMa context project

--- src/components/Aunty/Aunty.js
+++ src/components/Aunty/Aunty.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import { RingContext } from "../../App";
 
 const Aunty = (props) => {
   const { house } = props;
+  const ornaments = useContext(RingContext);
   return (
     <div>
       <h2>THis is Aunty.</h2>
       <h5>House: {house}</h5>
+      {ornaments}
     </div>
   );
 };
